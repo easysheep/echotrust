673af42ac14c2e3e7f794cda29924dc98096d6bf
easysheep <112809366+easysheep@users.noreply.github.com>
2024-10-30 14:12:51 +0530

    twelveth commit

--- middleware.js
+++ middleware.js
@@ -1,23 +1,8 @@
-// import { clerkMiddleware } from '@clerk/nextjs/server'
-
-// export default clerkMiddleware({
-//   publicRoutes:['/']
-// });
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// }
-
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware({
   // Make the home route ("/") public
-  publicRoutes: ['/', '/public-page', '/another-public-page'] // Add more public routes if needed
+  publicRoutes: ['/','/dashboard', '/dashboard/:echoId'] // Add more public routes if needed
 });
 
 export const config = {
